d0edc6a5e7de072b9628506f019995c83bc1a898
shonya3 <79873939+shonya3@users.noreply.github.com>
2022-07-10 20:43:56 +0300

    heroku-ready-9

--- src/database/db.ts
+++ src/database/db.ts
@@ -3,11 +3,13 @@ import { configENV } from '../configENV.js';
 configENV();
 
 const pool = new pg.Pool({
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  port: Number(process.env.POSTGRES_PORT),
+  // user: process.env.POSTGRES_USER,
+  // host: process.env.POSTGRES_HOST,
+  // database: process.env.POSTGRES_DB,
+  // password: process.env.POSTGRES_PASSWORD,
+  // port: Number(process.env.POSTGRES_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 });
 
 export default pool;
