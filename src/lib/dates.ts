081d2c92b2cfbb0a42863a2923f0f273d04149f2
shonya3 <79873939+shonya3@users.noreply.github.com>
2022-07-12 09:48:51 +0300

    tests for dates and general utils

--- src/lib/dates.ts
+++ src/lib/dates.ts
@@ -9,13 +9,14 @@ export const daysToMs = (num: number = 1) => hoursToMs(num) * 24;
 export const daysBefore = (date: Date, days: number) => new Date(date.getTime() - daysToMs(days));
 export const minsBefore = (date: Date, mins: number) => new Date(date.getTime() - minutesToMs(mins));
 export const minsLater = (date: Date, mins: number) => new Date(date.getTime() + minutesToMs(mins));
+export const secondsLater = (date: Date, seconds: number) => new Date(date.getTime() + 1000 * seconds);
 
 export const timeLeft = (date: Date) => {
   const now = new Date();
   if (date < now) return `Событие уже произошло`;
   const milliseconds = date.getTime() - now.getTime();
   const hour = Math.floor(milliseconds / hoursToMs());
-  const minute = Math.floor((milliseconds / minutesToMs()) % 60);
+  const minute = Math.ceil((milliseconds / minutesToMs()) % 60);
   const hourString = hour > 0 ? `${hour}ч` : '';
   const minuteString = `${minute}мин`;
   return `Осталось ${hourString}${minuteString}`;
@@ -31,13 +32,6 @@ export const toMs = ({ sec = 0, min = 0, hour = 0, day = 0 }: Time) => {
 export const waitMinutes = (minutes: number) => new Promise(r => setTimeout(r, minutesToMs(minutes)));
 export const waitSeconds = (seconds: number) => new Promise(r => setTimeout(r, secondsToMs(seconds)));
 
-export const relativeTimeFormat = (min: number, lang = 'ru') => {
-  const unit = min > 60 ? 'hour' : 'minute';
-  const val = unit === 'minute' ? min : Number((min / 60).toFixed(1));
-  const rtf = new Intl.RelativeTimeFormat(lang, { style: 'narrow' });
-  return rtf.format(val, unit);
-};
-
 export const cronToClosestDate = (expr: CronExpression) => cronParser.parseExpression(expr).next().toDate();
 export const dateToRule = (date: Date, tz = 'Europe/Moscow') => ({
   minute: date.getMinutes(),
@@ -46,10 +40,8 @@ export const dateToRule = (date: Date, tz = 'Europe/Moscow') => ({
   second: date.getSeconds(),
   tz,
 });
-export const timeString = (date: Date) => date.toLocaleTimeString('ru');
+export const timeString = (date: Date, locale = 'ru') => date.toLocaleTimeString(locale);
 
-export const addMinutesToDate = (date: Date, minutes: number) => new Date(date.getTime() + 1000 * 60 * minutes);
-export const addSecondsToDate = (date: Date, seconds: number) => new Date(date.getTime() + 1000 * seconds);
 export const unitToMs = (value: number, from: TimeUnit) => {
   switch (from) {
     case 'milliseconds':
@@ -102,14 +94,8 @@ export class TimeUnitConverter {
     return this.#value;
   }
 
-  c(val: number, unit?: TimeUnit) {
-    this.value = val;
-    if (unit) this.unit = unit;
-    return this;
-  }
-
   from(unit: TimeUnit) {
-    if (this.unit !== unit) this.unit = unit;
+    this.unit = unit;
     return this;
   }
 
@@ -119,4 +105,101 @@ export class TimeUnitConverter {
     this.unit = unit;
     return this.value;
   }
+}
+
+if (import.meta.vitest) {
+  const { it, expect, describe, vi } = import.meta.vitest;
+  const now = new Date();
+  const testDate = new Date(2022, 6, 11, 19, 10);
+
+  it('secondsToMs', () => {
+    expect(secondsToMs(5)).toBe(5 * 1000);
+    expect(secondsToMs(0)).toBe(0);
+  });
+  it('minutesToMs', () => {
+    expect(minutesToMs(5)).toBe(1000 * 60 * 5);
+  });
+  it('hoursToMs', () => {
+    expect(hoursToMs(1)).toBe(1000 * 60 * 60);
+  });
+  it('daysToMs', () => {
+    expect(daysToMs(100)).toBe(100 * 24 * 1000 * 60 * 60);
+  });
+  it('daysBefore', () => {
+    expect(now.getTime() - daysBefore(now, 5).getTime()).toBe(432000000);
+    expect(now.getTime() - daysBefore(now, 0).getTime()).toBe(0);
+  });
+  it('minsBefore', () => {
+    expect(now.getTime() - minsBefore(now, 5).getTime()).toBe(5 * 60 * 1000);
+  });
+  it('minsLater', () => {
+    expect(minsLater(now, 1).getTime() - now.getTime()).toBe(1000 * 60);
+  });
+  it('secondsLater', () => {
+    expect(secondsLater(now, 1).getTime() - now.getTime()).toBe(1000);
+  });
+  describe('timeLeft', () => {
+    it('checks if date refers to past', () => {
+      const before = new Date(now.getTime() - 1);
+      expect(timeLeft(before)).toBe('Событие уже произошло');
+    });
+    it('should return a ready-to-use formatted string with time left', () => {
+      const date = minsLater(now, 90);
+      const dayLater = minsLater(now, 60 * 24 + 30);
+      expect(timeLeft(date)).toBe('Осталось 1ч30мин');
+      expect(timeLeft(dayLater)).toBe('Осталось 24ч30мин');
+    });
+  });
+  it('minutesLeft', () => {
+    const tenMinsLater = minsLater(now, 10);
+    expect(Math.ceil(minutesLeft(tenMinsLater))).toBe(10);
+  });
+  it('toMs', () => {
+    expect(toMs({ sec: 50, min: 90, hour: 1 })).toBe(9050000);
+    expect(toMs({})).toBe(0);
+  });
+  it('cronToClosestDate', () => {
+    vi.useFakeTimers();
+    vi.setSystemTime(testDate);
+    const cron = '0 23 * * 2';
+    const date = cronToClosestDate(cron);
+    const dayOfMonth = date.getDate();
+    const hour = date.getHours();
+    expect(dayOfMonth).toBe(12);
+    expect(hour).toBe(23);
+
+    vi.useRealTimers();
+  });
+  it('timeString', () => {
+    expect(timeString(testDate)).toBe('19:10:00');
+  });
+  it('dateToRule', () => {
+    expect(dateToRule(testDate, 'Europe/Moscow')).toEqual({
+      minute: 10,
+      hour: 19,
+      dayOfWeek: 1,
+      tz: 'Europe/Moscow',
+      second: 0,
+    });
+  });
+  it('unitToMs', () => {
+    expect(unitToMs(10, 'milliseconds')).toBe(10);
+    expect(unitToMs(10, 'seconds')).toBe(10 * 1000);
+    expect(unitToMs(10, 'minutes')).toBe(10 * 1000 * 60);
+    expect(unitToMs(10, 'hours')).toBe(10 * 1000 * 60 * 60);
+    expect(unitToMs(10, 'days')).toBe(10 * 1000 * 60 * 60 * 24);
+  });
+  it('msToUnit', () => {
+    const v = 3_600_000;
+    expect(msToUnit(v, 'milliseconds')).toBe(3_600_000);
+    expect(msToUnit(v, 'seconds')).toBe(3_600);
+    expect(msToUnit(v, 'minutes')).toBe(60);
+    expect(msToUnit(v, 'hours')).toBe(1);
+    expect(msToUnit(v, 'days')).toBe(1 / 24);
+  });
+  describe('TimeUnitConverter', () => {
+    it('works correctly', () => expect(new TimeUnitConverter(10, 'minutes').to('seconds')).toBe(600));
+    it('defaults to milliseconds', () => expect(new TimeUnitConverter(1000).to('seconds')).toBe(1));
+    it('from - to', () => expect(new TimeUnitConverter(3600).from('seconds').to('hours')).toBe(1));
+  });
 }
--- src/lib/general.ts
+++ src/lib/general.ts
@@ -1,10 +1,13 @@
 import { resolve, dirname, extname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { writeFile } from 'node:fs/promises';
-import { AnyFunction, Wrapper } from '../types.js';
+import { AnyFunction } from '../types.js';
 
 export const unique = <T>(arr: T[]): T[] => Array.from(new Set(arr));
-export const extnameJsTs = (name: string) => extname(name) === '.js' || extname(name) === '.ts';
+export const extnameJsTs = (name: string) => {
+  if (['.spec', '.test'].some(e => name.includes(e))) return false;
+  return extname(name) === '.js' || extname(name) === '.ts';
+};
 export const __dir = (importMetaUrl: string) => dirname(fileURLToPath(importMetaUrl));
 export const resolveFrom__dirname = (importMetaUrl: string, path: string) => resolve(__dir(importMetaUrl), path);
 
@@ -27,16 +30,11 @@ export const capitalize = (str: string) => {
   return `${first.toUpperCase()}${rest.join('')}`;
 };
 
-export const countUniqueMessages = (messages: string[]) => {
-  const unique = Array.from(new Set(messages)).length;
-  return `unique messages: ${unique} / ${messages.length}`;
-};
-
 export const concatStringsToLength = (s1: string, s2: string, length: number) => {
   const s1Length = s1.length;
   const s2Length = s2.length;
   const neededSpaces = length - (s1Length + s2Length);
-  return `${s2}${' '.repeat(neededSpaces >= 0 ? neededSpaces : 0)}${s1}`;
+  return `${s1}${' '.repeat(neededSpaces >= 0 ? neededSpaces : 0)}${s2}`;
 };
 
 export const preventSpamWrapper = <T extends AnyFunction>(
@@ -55,3 +53,48 @@ export const preventSpamWrapper = <T extends AnyFunction>(
     return result;
   };
 };
+
+if (import.meta.vitest) {
+  const { it, expect, describe, vi } = import.meta.vitest;
+
+  it('unique', () => {
+    expect(unique([1, 2, 2, 2, 2, 2, 2, 1, 3, 3, 3, 3, 3])).toEqual([1, 2, 3]);
+  });
+  it('extnameJsTs', () => {
+    expect(extnameJsTs('index.html')).toBe(false);
+    expect(extnameJsTs('index.js')).toBe(true);
+    expect(extnameJsTs('index.ts')).toBe(true);
+    expect(extnameJsTs('index.spec.py')).toBe(false);
+    expect(extnameJsTs('index.spec.ts')).toBe(false);
+    expect(extnameJsTs('index.test.js')).toBe(false);
+  });
+
+  it('throwIfNot', () => {
+    let res;
+    let errorMsg = 'Send this error message to test function';
+    try {
+      throwIfNot(process.env, 'NOTHERE', errorMsg);
+    } catch (err) {
+      res = errorMsg;
+    }
+    expect(res).toBe(errorMsg);
+  });
+
+  it('capitalize', () => {
+    expect(capitalize('filename')).toBe('Filename');
+    expect(capitalize('f')).toBe('F');
+    expect(capitalize('')).toBe('');
+  });
+  it('concatStringsToLength', () => {
+    const testString = concatStringsToLength('paragraph', 'article', 30);
+    expect(testString.length).toBe(30);
+    expect(testString).toBe('paragraph              article');
+  });
+  it('preventSpamWrapper', () => {
+    const arr = [];
+    const addArrElement = () => arr.push(1);
+    const wrapped = preventSpamWrapper(addArrElement);
+    for (let i = 0; i < 10; i++) wrapped();
+    expect(arr.length).toBe(1);
+  });
+}
