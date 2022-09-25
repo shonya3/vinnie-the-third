88f062a22b1f96d0e8d9b6f85d261e3679a264ae
= <=>
2022-09-25 17:37:37 +0300

    add msg reaction

--- src/bot/handlers/messageHandler/index.ts
+++ src/bot/handlers/messageHandler/index.ts
@@ -1,11 +1,12 @@
 import { Message } from 'discord.js';
 
 export const messageHandler = async (msg: Message) => {
-  try {
-    if (msg.content.includes('nivazmojna')) await msg.react('<:nivazmojna:850123166923882558>');
-    if (msg.content.includes('jaba')) await msg.react('<:jaba:637684829114204175>');
-  } catch (err) {
-    console.log('Emoji reaction error');
-    console.log(err);
-  }
+	try {
+		if (msg.content.includes('nivazmojna')) await msg.react('<:nivazmojna:850123166923882558>');
+		if (msg.content.includes('jaba')) await msg.react('<:jaba:637684829114204175>');
+		if (msg.content.toLowerCase().includes('утреч')) await msg.react('🤓');
+	} catch (err) {
+		console.log('Emoji reaction error');
+		console.log(err);
+	}
 };
