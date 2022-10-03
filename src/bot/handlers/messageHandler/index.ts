4d4c1db28bb3bb5babd29e381c649cff03ca3d33
= <=>
2022-10-03 21:10:33 +0300

    Message handler patterns

--- src/bot/handlers/messageHandler/index.ts
+++ src/bot/handlers/messageHandler/index.ts
@@ -2,9 +2,15 @@ import { Message } from 'discord.js';
 
 export const messageHandler = async (msg: Message) => {
 	try {
+		console.log(msg.content.charCodeAt(0).toString(16), msg.content.charCodeAt(1).toString(16));
+		console.log(msg.content.charCodeAt(0), msg.content.charCodeAt(1));
 		if (msg.content.includes('nivazmojna')) await msg.react('<:nivazmojna:850123166923882558>');
 		if (msg.content.includes('jaba')) await msg.react('<:jaba:637684829114204175>');
-		if (msg.content.toLowerCase().includes('утреч')) await msg.react('🤓');
+
+		const utrechka = ['утреч', 'yтреч', 'утpеч', 'утрeч', 'yтpеч', 'yтрeч', 'утpeч', 'yтpeч'].some(variant =>
+			msg.content.toLowerCase().includes(variant)
+		);
+		if (utrechka) await msg.react('🤓');
 	} catch (err) {
 		console.log('Emoji reaction error');
 		console.log(err);
