756aadbcfb078f474a8b80bf358032c21ebeff0d
shonya3 <poeshonya3@gmail.com>
2023-06-02 18:15:29 +0300

    optimize generateBossMap

--- src/modules/bossSchedule/generateBossMap.ts
+++ src/modules/bossSchedule/generateBossMap.ts
@@ -1,35 +1,20 @@
 import { cronToClosestDate } from '../../lib/dates.js';
-
-import type { BossWithCron, BossWithDate } from '../../types.js';
 import { table } from './table.js';
 
 export const generateBossMap = (
 	{ nameStyle }: { nameStyle: 'full' | 'short' } = { nameStyle: 'short' }
 ): Map<number, string[]> => {
-	const bossWithDateArray: BossWithDate[] = [];
+	const map: Map<EpochTimeStamp, string[]> = new Map();
 
 	for (const boss of table) {
+		const name = nameStyle === 'short' ? boss.shortName : boss.name;
 		for (const cron of boss.cronExpressions) {
-			bossWithDateArray.push({
-				name: boss.name,
-				shortName: boss.shortName,
-				respawnDate: cronToClosestDate(cron),
-			});
+			const timestamp = cronToClosestDate(cron).getTime();
+			const bossNames = map.get(timestamp) ?? [];
+			bossNames.push(name);
+			map.set(timestamp, bossNames);
 		}
 	}
 
-	bossWithDateArray.sort((a, b) => a.respawnDate.getTime() - b.respawnDate.getTime());
-
-	const map: Map<EpochTimeStamp, string[]> = new Map();
-
-	bossWithDateArray.forEach(boss => {
-		const timestamp = boss.respawnDate.getTime();
-		const bossNames = map.get(timestamp) ?? [];
-
-		const name = nameStyle === 'short' ? boss.shortName : boss.name;
-		bossNames.push(name);
-		map.set(timestamp, bossNames);
-	});
-
-	return map;
+	return new Map(Array.from(map).sort((a, b) => a[0] - b[0]));
 };
